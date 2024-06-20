7278a03150f6b2afce44bb66a0f88a6c95cfac48
hublot <hublot@aliyun.com>
2024-06-20 09:26:31 +0800

    feat: add podspec

---
+++ index.d.ts
@@ -0,0 +1,7 @@
+declare module 'react-native-webview-cleaner' {
+  interface IWebViewCleaner {
+    clearAll: () => void;
+  }
+  export const RNWebViewCleaner: IWebViewCleaner;
+  export default RNWebViewCleaner;
+}
